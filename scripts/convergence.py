713058e97734b1c8b830a78a40be3d02c8bfaf0e
Félix Laplante <75139846+felixlaplante0@users.noreply.github.com>
2026-05-10 18:17:23 +0200

    Update figures

--- scripts/convergence.py
+++ scripts/convergence.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import torch
 import zuko
-from pitcp import PITCP
 from scipy.stats import norm
+from pitcp import PITCP
 
 plt.rcParams.update(
     {
@@ -33,14 +34,14 @@ def score_abs(x, y):
 
 torch.manual_seed(42)
 
-(X_cal, y_cal), (X_test, y_test) = [gen_data(n) for n in (1000, 5000)]
+X_cal, y_cal = gen_data(1000)
 
 Ns = torch.linspace(0, 5000, 5, dtype=int)
-q = 0.9
+qs = np.linspace(0.0, 1.0, 20).tolist()
 n_runs = 5
 
 xv = torch.linspace(-1, 1, 500)
-yv = torch.linspace(y_test.min(), y_test.max(), 1000)
+yv = torch.linspace(-5, 5, 1000)
 xg, yg = torch.meshgrid(xv, yv, indexing="ij")
 
 Xg, Yg = xg.unsqueeze(-1), yg.unsqueeze(-1)
@@ -69,23 +70,22 @@ for _ in range(n_runs):
 
             batch_size = max(1, n.item() // 10)
             pitcp = PITCP(
-                score_abs, model, optimizer, n_epochs=100, batch_size=batch_size
+                score_abs, model, optimizer, n_epochs=25, batch_size=batch_size
             )
             if n.item() > 0:
                 pitcp.fit(X_train, y_train)
             pitcp.conformalize(X_cal, y_cal)
 
-            Zp = pitcp.predict(Xg, Yg, quantile=q).squeeze(-1).bool()
-            Cp = pitcp.predict(X_test, y_test, quantile=q).flatten()
+            Zp = pitcp.predict(Xg, Yg, quantile=qs).bool()
 
-            ymin = torch.where(Zp, yv, torch.inf).min(1).values
-            ymax = torch.where(Zp, yv, -torch.inf).max(1).values
+            ymin = torch.where(Zp, yv.view(1, -1, 1), 0.0).min(1).values
+            ymax = torch.where(Zp, yv.view(1, -1, 1), 0.0).max(1).values
 
-            marginal_coverage = Cp.float().mean().item()
-            coverage = norm.cdf((ymax / s).numpy()) - norm.cdf((ymin / s).numpy())
-            l1 = torch.nanmean(
-                torch.abs(torch.tensor(coverage) - marginal_coverage)
-            ).item()
+            coverage = norm.cdf((ymax / s.view(-1, 1)).numpy()) - norm.cdf(
+                (ymin / s.view(-1, 1)).numpy()
+            )
+            marginal_coverage = np.nanmean(coverage, axis=0)
+            l1 = np.nanmean(np.abs(coverage - marginal_coverage).max(axis=1))
             data.append({"Model": name, "N": n.item(), "MAE": l1})
 
 df = pd.DataFrame(data)
@@ -107,7 +107,7 @@ sns.lineplot(
 ax.set(
     title="Convergence of the PIT-CP procedure",
     xlabel="N (training samples)",
-    ylabel="MAE",
+    ylabel=r"$\mathbb{E}[\widehat{\Delta}(X)]$",
 )
 ax.legend()
 
